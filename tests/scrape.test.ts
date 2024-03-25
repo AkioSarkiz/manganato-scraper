ebba0bb42601ccbe8022975f50c555ea2165b3d0
DLaptiev <akiosarkiz@gmail.com>
2024-03-25 21:33:28 +0100

    remove old name of test

--- tests/scrape.test.ts
+++ tests/scrape.test.ts
@@ -2,7 +2,7 @@ import { getLatestManga } from "../src";
 import { describe, test, expect } from "vitest";
 
 describe("should load latest manga", () => {
-  test("dump test", async () => {
+  test(async () => {
     const result = await getLatestManga();
     expect(result).toBeInstanceOf(Array);
   });
