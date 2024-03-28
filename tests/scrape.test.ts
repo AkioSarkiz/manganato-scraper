6ddaa12c6efd76da38503a5a21d3cb0f787dcfa4
DLaptiev <akiosarkiz@gmail.com>
2024-03-28 08:50:25 +0100

    add more tests

--- tests/scrape.test.ts
+++ tests/scrape.test.ts
@@ -1,9 +1,19 @@
 import { getLatestManga } from "../src";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, expectTypeOf } from "vitest";
+import { LatestManga } from "../src/types";
 
 describe("should load latest manga", () => {
-  test(async () => {
+  test("without any props", async () => {
     const result = await getLatestManga();
     expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBeGreaterThanOrEqual(1);
+    expectTypeOf(result).toEqualTypeOf<LatestManga[]>();
+  });
+
+  test("a second page", async () => {
+    const result = await getLatestManga({ page: 2 });
+    expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBeGreaterThanOrEqual(1);
+    expectTypeOf(result).toEqualTypeOf<LatestManga[]>();
   });
 });
