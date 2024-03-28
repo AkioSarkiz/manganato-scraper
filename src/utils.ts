2cfbfff1c6fdfd7b5e160d243184972aadd6903b
DLaptiev <akiosarkiz@gmail.com>
2024-03-28 08:50:19 +0100

    update getBaseUrl util

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { BASE_URL } from "./config";
 import urlJoin from "url-join";
 
-export const getBaseUrl = (path: string): string => {
+export const getBaseUrl = (path: string = ""): string => {
   return urlJoin(BASE_URL, path);
 };
